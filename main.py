dddfdd63ea2ac7ac7c794f6d7736fd509b66fd32
deciever5 <marcinkunikowski@gmail.com>
2022-11-30 14:30:18 +0100

    added bootstrap template

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def homepage():
-    return render_template('index.html')
-
+    movies = [i for i   in range(6)]
+    return render_template("homepage.html", movies=movies)
 
 Api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODg4ZWYxN2I2MWRjZTgzMWZiNGEwYjdiMjA4YTliNCIsInN1YiI6IjYzODcwMzJjMjI5YWUyMTViNDYwNTc1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OYs7rxZFJzRMpix6mv1Bos89sX42SekoUY36kZAkJ_A"
 
