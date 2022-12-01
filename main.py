0a4bf3b6516a6b2e06cb61b2ff5b1026016d7230
deciever5 <marcinkunikowski@gmail.com>
2022-12-01 14:10:51 +0100

    added img url

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 
+from tmdb_client import get_popular_movies, get_poster_url
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
     movies = [i for i in range(6)]
+    get_poster_url("/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg")
     return render_template("homepage.html", movies=movies)
 
 
