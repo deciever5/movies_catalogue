77022aa7986362e6c997bd2ea92d9005500837a9
deciever5 <marcinkunikowski@gmail.com>
2022-12-06 14:17:23 +0100

    added movie details view

--- main.py
+++ main.py
@@ -8,21 +8,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies =tmdb_client.get_movies(how_many= 12)
+    movies = tmdb_client.get_movies(how_many=12)
     return render_template("homepage.html", movies=movies)
+
 
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id):
-    return render_template("movie_details.html")
+    details = tmdb_client.get_single_movie(movie_id)
+    return render_template("movie_details.html",movie = details)
+
 
 @app.context_processor
 def utility_processor():
     def tmdb_image_url(path, size):
         return tmdb_client.get_poster_url(path, size)
+
     return {"tmdb_image_url": tmdb_image_url}
 
-def movie_details(id):
-    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb_client.py
+++ tmdb_client.py
@@ -2,13 +2,13 @@ from random import shuffle
 
 import requests
 
-
-def get_popular_movies():
-    Api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODg4ZWYxN2I2MWRjZTgzMWZiNGEwYjdiMjA4YTliNCIsInN1YiI6IjYzODcwMzJjMjI5YWUyMTViNDYwNTc1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OYs7rxZFJzRMpix6mv1Bos89sX42SekoUY36kZAkJ_A"
-    headers = {
+Api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODg4ZWYxN2I2MWRjZTgzMWZiNGEwYjdiMjA4YTliNCIsInN1YiI6IjYzODcwMzJjMjI5YWUyMTViNDYwNTc1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OYs7rxZFJzRMpix6mv1Bos89sX42SekoUY36kZAkJ_A"
+headers = {
         "Authorization": f"Bearer {Api_token}",
         "Content-Type": "application/json; charset=utf-8"
     }
+
+def get_popular_movies():
     page = 'https://api.themoviedb.org/3/movie/popular'
     response = requests.get(page, headers=headers)
     return response.json()
@@ -23,3 +23,14 @@ def get_movies(how_many):
     data = get_popular_movies()
     shuffle(data["results"])
     return data["results"][:how_many]
+
+def get_single_movie(movie_id):
+    page = f'https://api.themoviedb.org/3/movie/{movie_id}'
+    response = requests.get(page, headers=headers)
+    return response.json()
+
+def get_movie_credits(movie_id):
+    page = f'https://developers.themoviedb.org/3/movies/{movie_id}/credits'
+    response = requests.get(page, headers=headers)
+    return response.json()
+
