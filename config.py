0499b79926a19276373d01de5f7fb0166753824d
deciever5 <marcinkunikowski@gmail.com>
2023-04-19 11:14:13 +0200

    moved token,headers and base urls to config file. Added exeption handling for api requests, added docstrings for functions

---
+++ config.py
@@ -0,0 +1,8 @@
+"""Token added for testing purposes. Please use your own token."""
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODg4ZWYxN2I2MWRjZTgzMWZiNGEwYjdiMjA4YTliNCIsInN1YiI6IjYzODcwMzJjMjI5YWUyMTViNDYwNTc1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OYs7rxZFJzRMpix6mv1Bos89sX42SekoUY36kZAkJ_A"
+HEADERS = {
+    "Content-Type": "application/json; charset=utf-8"
+}
+BASE_URL = "https://api.themoviedb.org/3"
+IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+NO_IMAGE_AVAILABLE = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
--- main.py
+++ main.py
@@ -1,37 +1,89 @@
-from random import choice
-
-from flask import Flask, render_template, request
-
-import tmdb_client
-
-app = Flask(__name__)
+from random import shuffle
+import requests
+from config import API_TOKEN, HEADERS, BASE_URL, IMAGE_BASE_URL, NO_IMAGE_AVAILABLE
 
 
-@app.route('/')
-def homepage():
-    selected_list = request.args.get('list_name', 'popular')
-    if selected_list not in ['popular', 'top_rated', 'upcoming', 'now_playing']:
-        selected_list = 'popular'
-    movies = tmdb_client.get_movies(how_many=12, list_name=selected_list)
-    return render_template("homepage.html", movies=movies, current_list=selected_list)
+def get_movie_list(list_name):
+    """
+    Retrieve a list of movies based on the given list name.
+    """
+    url = f'{BASE_URL}/movie/{list_name}'
+    params = {"api_key": API_TOKEN}
+    try:
+        response = requests.get(url, headers=HEADERS, params=params)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return None
+
+    return response.json()
 
 
-@app.route("/movie/<movie_id>")
-def movie_details(movie_id):
-    details = tmdb_client.get_single_movie(movie_id)
-    cast = tmdb_client.get_movie_credits(movie_id)
-    movie_images = tmdb_client.get_movie_images(movie_id)
-    selected_backdrop = choice(movie_images['backdrops'])
-    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
+def get_image_url(poster_api_path, size="w342"):
+    """
+    Generate the image URL from the poster API path.
+    """
+    if poster_api_path is None:
+        return NO_IMAGE_AVAILABLE
+
+    return IMAGE_BASE_URL + size + poster_api_path
 
 
-@app.context_processor
-def utility_processor():
-    def tmdb_image_url(path, size):
-        return tmdb_client.get_image_url(path, size)
+def get_movies(how_many, list_name="popular"):
+    """
+    Retrieve and shuffle a list of movies, returning the specified number of them.
+    """
+    movie_data = get_movie_list(list_name)
+    if movie_data is None:
+        return []
 
-    return {"tmdb_image_url": tmdb_image_url}
+    shuffle(movie_data["results"])
+    return movie_data["results"][:how_many]
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def get_single_movie(movie_id):
+    """
+    Retrieve details for a single movie.
+    """
+    url = f'{BASE_URL}/movie/{movie_id}'
+    params = {"api_key": API_TOKEN}
+    try:
+        response = requests.get(url, headers=HEADERS, params=params)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return None
+
+    return response.json()
+
+
+def get_movie_credits(movie_id):
+    """
+    Retrieve the cast credits for a movie.
+    """
+    url = f'{BASE_URL}/movie/{movie_id}/credits'
+    params = {"api_key": API_TOKEN}
+    try:
+        response = requests.get(url, headers=HEADERS, params=params)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return []
+
+    return response.json()['cast']
+
+
+def get_movie_images(movie_id):
+    """
+    Retrieve the images associated with a movie.
+    """
+    url = f"{BASE_URL}/movie/{movie_id}/images"
+    params = {"api_key": API_TOKEN}
+    try:
+        response = requests.get(url, headers=HEADERS, params=params)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return {}
+
+    return response.json()
